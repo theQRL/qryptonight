f64d8cd33078b4bd12a358f19f5cbdcd34507118
Juan Leni <lenijuan@gmail.com>
2018-04-24 16:27:27 +0200

    Adding python tests

--- tests/python/test_qryptominer.py
+++ tests/python/test_qryptominer.py
@@ -3,6 +3,7 @@
 from unittest import TestCase
 import time
 
+from pyqryptonight import pyqryptonight
 from pyqryptonight.pyqryptonight import Qryptominer
 from pyqryptonight.pyqryptonight import Qryptonight
 from pyqryptonight.pyqryptonight import PoWHelper
@@ -17,9 +18,10 @@ class TestQryptominer(TestCase):
             def __init__(self):
                 Qryptominer.__init__(self)
 
-            def solutionEvent(self, nonce):
-                print("Hey a solution has been found!", nonce)
-                self.python_nonce = nonce
+            def handleEvent(self, event):
+                if event.type == pyqryptonight.SOLUTION:
+                    print("Hey a solution has been found!", event.nonce)
+                    self.python_nonce = event.nonce
 
         input_bytes = [
             0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
@@ -62,9 +64,10 @@ class TestQryptominer(TestCase):
             def __init__(self):
                 Qryptominer.__init__(self)
 
-            def solutionEvent(self, nonce):
-                print("Hey a solution has been found!", nonce)
-                self.python_nonce = nonce
+            def handleEvent(self, event):
+                if event.type == pyqryptonight.SOLUTION:
+                    print("Hey a solution has been found!", event.nonce)
+                    self.python_nonce = event.nonce
 
         input_bytes = [
             0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
@@ -115,3 +118,137 @@ class TestQryptominer(TestCase):
         self.assertTrue(ph.verifyInput(solution_input, target))
         solution_input[4] = 0x29
         self.assertFalse(ph.verifyInput(solution_input, target))
+
+    def test_miner_timeout(self):
+        class CustomQMiner(Qryptominer):
+            def __init__(self):
+                Qryptominer.__init__(self)
+                self.timeout_triggered = False
+
+            def handleEvent(self, event):
+                if event.type == pyqryptonight.TIMEOUT:
+                    print("Timeout")
+                    self.timeout_triggered = True
+
+        input_bytes = [
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09
+        ]
+        target = [
+            0x1E, 0xE5, 0x3F, 0xE1, 0xAC, 0xF3, 0x55, 0x92,
+            0x66, 0xD8, 0x43, 0x89, 0xCE, 0xDE, 0x99, 0x33,
+            0xC6, 0x8F, 0xC5, 0x1E, 0xD0, 0xA6, 0xC7, 0x91,
+            0xF8, 0xF9, 0xE8, 0x9D, 0x00, 0x00, 0x00, 0x00
+        ]
+
+        # Create a customized miner
+        qm = CustomQMiner()
+
+        # Set input bytes, nonce
+        qm.start(input=input_bytes,
+                 nonceOffset=0,
+                 target=target,
+                 thread_count=2)
+
+        qm.setTimer(2000)
+
+        # Python can sleep or do something else.. the callback will happen in the background
+        time.sleep(5)
+
+        # This property has been just created in the python custom class when the event is received
+        self.assertTrue(qm.timeout_triggered)
+
+    def test_miner_timeout_extend(self):
+        class CustomQMiner(Qryptominer):
+            def __init__(self):
+                Qryptominer.__init__(self)
+                self.timeout_triggered = False
+
+            def handleEvent(self, event):
+                if event.type == pyqryptonight.TIMEOUT:
+                    print("Timeout")
+                    self.timeout_triggered = True
+
+        input_bytes = [
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09
+        ]
+        target = [
+            0x1E, 0xE5, 0x3F, 0xE1, 0xAC, 0xF3, 0x55, 0x92,
+            0x66, 0xD8, 0x43, 0x89, 0xCE, 0xDE, 0x99, 0x33,
+            0xC6, 0x8F, 0xC5, 0x1E, 0xD0, 0xA6, 0xC7, 0x91,
+            0xF8, 0xF9, 0xE8, 0x9D, 0x00, 0x00, 0x00, 0x00
+        ]
+
+        # Create a customized miner
+        qm = CustomQMiner()
+
+        # Set input bytes, nonce
+        qm.start(input=input_bytes,
+                 nonceOffset=0,
+                 target=target,
+                 thread_count=2)
+
+        qm.setTimer(2000)
+        time.sleep(1)
+        qm.setTimer(4000)
+        time.sleep(3)
+
+        # This property has been just created in the python custom class when the event is received
+        self.assertFalse(qm.timeout_triggered)
+
+    def test_miner_timeout_disable(self):
+        class CustomQMiner(Qryptominer):
+            def __init__(self):
+                Qryptominer.__init__(self)
+                self.timeout_triggered = False
+
+            def handleEvent(self, event):
+                if event.type == pyqryptonight.TIMEOUT:
+                    print("Timeout")
+                    self.timeout_triggered = True
+
+        input_bytes = [
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09,
+            0x03, 0x05, 0x07, 0x09, 0x03, 0x05, 0x07, 0x09
+        ]
+        target = [
+            0x1E, 0xE5, 0x3F, 0xE1, 0xAC, 0xF3, 0x55, 0x92,
+            0x66, 0xD8, 0x43, 0x89, 0xCE, 0xDE, 0x99, 0x33,
+            0xC6, 0x8F, 0xC5, 0x1E, 0xD0, 0xA6, 0xC7, 0x91,
+            0xF8, 0xF9, 0xE8, 0x9D, 0x00, 0x00, 0x00, 0x00
+        ]
+
+        # Create a customized miner
+        qm = CustomQMiner()
+
+        # Set input bytes, nonce
+        qm.start(input=input_bytes,
+                 nonceOffset=0,
+                 target=target,
+                 thread_count=2)
+
+        qm.setTimer(500)
+        qm.disableTimer()
+        time.sleep(1)
+
+        # This property has been just created in the python custom class when the event is received
+        self.assertFalse(qm.timeout_triggered)
